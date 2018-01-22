1486a8cf7a0a255f30965b2eae2f0b279c896501
3223779RC <matadic@infoseek.jp>
2018-01-22 23:28:35 +0900

    Update for xml documents

--- lib/smile_filter/filter.rb
+++ lib/smile_filter/filter.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 require 'smile_filter/filter_file_parser'
-require 'smile_filter/comment_data'
+require 'smile_filter/comment_data_xml'
+require 'smile_filter/comment_data_json'
 require 'smile_filter/error_handler'
 
 module SmileFilter
   class Filter
-    def self.exec(json)
-      new(CommentData.new(json)).exec.to_json(space: ' ')
+    def self.exec(res)
+      comment_data = case res.content_type
+                     when /json/ then CommentDataJSON.new(res.body)
+                     when /xml/  then CommentDataXML.new(res.body)
+                     end
+      new(comment_data).exec.to_document
     end
     
     def initialize(comment_data)
