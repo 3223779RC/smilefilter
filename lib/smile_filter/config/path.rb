01eb772dab49f11d21e6bac0eda6046ae22f85f4
3223779RC <matadic@infoseek.jp>
2018-01-25 09:23:49 +0900

    Remove parts depending on Ocra

--- lib/smile_filter/config/path.rb
+++ lib/smile_filter/config/path.rb
@@ -4,15 +4,12 @@ module SmileFilter
   module Config
     module Path
       ROOT              = File.expand_path('../../../../', __FILE__)
-      if ENV.key?('OCRA_EXECUTABLE')
-        EXE_ROOT        = File.dirname(ENV['OCRA_EXECUTABLE']).gsub('\\', '/')
-      end
       DEFAULT_CONFIG    = "#{ROOT}/lib/smile_filter/config/default.yml"
       DEFAULT_FILTER    = "#{ROOT}/lib/smile_filter/config/default_filter.rb"
       DEFAULT_LIST_FILE = "#{ROOT}/lib/smile_filter/config/default_list.txt"
       TEMPLATE_PAC_FILE = "#{ROOT}/lib/smile_filter/config/template.pac"
+      PAC_FILE          = "#{Path::ROOT}/public_html/proxy.pac"
       DOCUMENT_ROOT     = Initializer.document_root
-      PAC_FILE          = Initializer.pac_file
       USER_DIRECTORY    = Initializer.user_directory
       LIST_FILE         = "#{USER_DIRECTORY}/list.txt"
       USER_FILTER       = "#{USER_DIRECTORY}/filter.rb"
