22156bbe9615c1287e02e8eca90c89276b662fb0
3223779RC <matadic@infoseek.jp>
2018-03-04 17:18:11 +0900

    Tweak

--- lib/smile_filter/config/path.rb
+++ lib/smile_filter/config/path.rb
@@ -3,16 +3,16 @@
 module SmileFilter
   module Config
     module Path
-      ROOT              = File.expand_path('../../../../', __FILE__)
-      DEFAULT_CONFIG    = "#{ROOT}/lib/smile_filter/config/default.yml"
-      DEFAULT_FILTER    = "#{ROOT}/lib/smile_filter/config/default_filter.rb"
-      DEFAULT_LIST_FILE = "#{ROOT}/lib/smile_filter/config/default_list.txt"
-      TEMPLATE_PAC_FILE = "#{ROOT}/lib/smile_filter/config/template.pac"
-      PAC_FILE          = "#{Path::ROOT}/public_html/proxy.pac"
-      DOCUMENT_ROOT     = Initializer.document_root
-      USER_DIRECTORY    = Initializer.user_directory
-      USER_CONFIG       = "#{USER_DIRECTORY}/config.yml"
-      LOG               = "#{USER_DIRECTORY}/log"
+      ROOT               = File.expand_path('../../../../', __FILE__)
+      DEFAULT_CONFIG     = "#{ROOT}/lib/smile_filter/config/default.yml"
+      DEFAULT_FILTER_RB  = "#{ROOT}/lib/smile_filter/config/default_filter.rb"
+      DEFAULT_FILTER_TXT = "#{ROOT}/lib/smile_filter/config/default_filter.txt"
+      TEMPLATE_PAC_FILE  = "#{ROOT}/lib/smile_filter/config/template.pac"
+      PAC_FILE           = "#{Path::ROOT}/public_html/proxy.pac"
+      DOCUMENT_ROOT      = Initializer.document_root
+      USER_DIRECTORY     = Initializer.user_directory
+      USER_CONFIG        = "#{USER_DIRECTORY}/config.yml"
+      LOG                = "#{USER_DIRECTORY}/log"
       
       class << self
         def filter(mode)
