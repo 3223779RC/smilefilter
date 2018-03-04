b46cbeb2e81390b4a3b960ed287ff983636277de
3223779RC <matadic@infoseek.jp>
2018-03-04 17:27:49 +0900

    Update for interaction mode

--- lib/smile_filter/config/default_filter.rb
+++ lib/smile_filter/config/default_filter.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: false
 
+#  UserFilter.#exec が定義されると、rbファイルのフィルターが
+#  txtファイルのフィルターに先立って処理されるようになります。
+#  例として、これより下の行の#を削除すると、
+#  一度でもbigかつshitaでコメントしたユーザーの全てのコメントを
+#  smallかつblackかつnakaに変更します。
+
 module SmileFilter
   module UserFilter
     module_function
-  #  UserFilter.#exec が定義されると list.txt に先立って処理されます。
   # def exec(chats)
-  #   「使用例」の◇はここに挿入
+  #   black_list = chats.each_with_object([]) do |chat, ids|
+  #                if chat.position?(:shita) && chat.size?(:big)
+  #                   ids << chat.user_id
+  #                 end
+  #               end
+  #   black_list.uniq!
   #   chats.each do |chat|
-  #     next if chat.empty?
-  #       「使用例」の〇はここに挿入
+  #     next unless black_list.include?(chat.user_id)
+  #     chat.site = :small
+  #     chat.color = :black
+  #     chat.position = :naka
   #   end
   # end
   end
